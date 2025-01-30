ca9ea542563538a1e792b1ef34d8d8891c570a50
Faokunn <157444535+Faokunn@users.noreply.github.com>
2025-01-30 14:20:26 +0800

    vercel

--- models/user_model.py
+++ models/user_model.py
@@ -10,7 +10,7 @@ class User(Base):
     email = Column(String(120), unique=True, nullable=False)
     password = Column(String(128), nullable=False)
 
-    notifications = relationship("Notification", back_populates="user", cascade="all, delete-orphan")
+    notifications = relationship("Notification", back_populates="users", cascade="all, delete-orphan")
     contacts = relationship("ContactModel", back_populates="user", cascade="all, delete-orphan")
     sos_alerts = relationship("SOSAlerter", back_populates="user", cascade="all, delete-orphan")
     profile = relationship("Profile", back_populates="user", uselist=False, cascade="all, delete-orphan")
