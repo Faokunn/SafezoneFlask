848b8cd76ca1b43603f110684a42fa3829ea54d6
Faokunn <157444535+Faokunn@users.noreply.github.com>
2025-02-10 16:35:04 +0800

    Vercel Update

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from database.base import Base, engine
 from flasgger import Swagger
 from flask_cors import CORS
 
-
-
 from models.user_model import User
 from models.sosalerts_model import SOSAlerter
 from models.safezone_model import SafeZone
