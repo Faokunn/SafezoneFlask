2ae2c4ca1d433170e7561f0c9d93347c5183b7d4
Faokunn <miro.asuncion.up@phinmaed.com>
2025-02-12 16:44:24 +0800

    vERCEL fIX

--- models/circle_model.py
+++ models/circle_model.py
@@ -5,7 +5,6 @@ from database.base import Base
 class Circle(Base):
     __tablename__ = 'circle'
 
-
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
